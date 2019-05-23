06a1962322182b1002785bf6fc443fb51d252d12
romenigld <romenigld@gmail.com>
2019-05-23 16:32:08 +0200

    Phoenix 1.4.6 Framework Guides - Views

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -7,17 +7,29 @@ defmodule HelloWeb.PageController do
     assign(conn, :default_message, msg)
   end
 
+  def show(conn, _params) do
+    page = %{title: "foo"}
+    render(conn, "show.json", page: page)
+  end
+
   def index(conn, _params) do
+    pages = [%{title: "foo"}, %{title: "bar"}]
+    render(conn, "index.json", pages: pages)
 
-    conn
-    |> put_layout("admin.html")
-    |> assign(:message, "'Welcome by controller!'")
-    |> render("index.html")
+    # conn
+    # |> assign(:message, "'Using a new function title() in the layout view!'")
+    # |> render("index.html")
 
+    # conn
+    # |> put_layout("admin.html")
+    # |> assign(:message, "'Welcome by controller!'")
+    # |> render("index.html")
 
+    # conn
      # |> put_resp_content_type("text/plain")
      # |> send_resp(201, "") # send a response with a status of "201" with no body whatsoever and show us a completely blank page.
 
+    # conn
     # |> assign(:message, "'Welcome by controller!'")
     # |> put_flash(:info, "Welcome to Phoenix, from flash info!")
     # |> put_flash(:error, "Let's pretend we have an error.")
--- lib/hello_web/views/page_view.ex
+++ lib/hello_web/views/page_view.ex
@@ -1,3 +1,63 @@
 defmodule HelloWeb.PageView do
   use HelloWeb, :view
+  alias HelloWeb.AuthorView
+
+  def render("page_with_authors.json", %{page: page}) do
+    %{title: page.title,
+      authors: render_many(page.authors, AuthorView, "author.json", as: :writer)}
+  end
+
+  def render("index.json", %{pages: pages}) do
+    %{data: render_many(pages, HelloWeb.PageView, "page.json")}
+  end
+
+  def render("show.json", %{page: page}) do
+    %{data: render_one(page, HelloWeb.PageView, "page.json")}
+  end
+
+  def render("page.json", %{page: page}) do
+    %{title: page.title}
+  end
+  # def render("index.html", assigns) do
+  #   "rendering with assigns #{inspect Map.keys(assigns)}"
+  # end
+
+  def message do
+    "Hello from the view!"
+  end
+
 end
+
+# Testing
+#   iex(5)> Phoenix.View.render(HelloWeb.PageView, "test.html", %{})
+# {:safe, ["\nThis is the message: ", "Hello from the view!", "\n"]}
+
+#   iex(6)> r HelloWeb.PageView
+#   warning: redefining module HelloWeb.PageView (current version loaded from _build/dev/lib/hello/ebin/Elixir.HelloWeb.PageView.beam)
+#     lib/hello_web/views/page_view.ex:1
+#   {:reloaded, HelloWeb.PageView, [HelloWeb.PageView]}
+
+#   iex(7)> Phoenix.View.render(HelloWeb.PageView, "test.html", message: "Assigns has an @.")
+# {:safe,
+#  ["I came from assigns: ", "Assigns has an @.", "\nThis is the message: ",
+#   "Hello from the view!", "\n"]}
+
+# Test out the HTML escaping
+#   iex(14)> Phoenix.View.render(HelloWeb.PageView, "test.html", message: "<script>badThings();</script>")
+# {:safe,
+#  [
+#    "I came from assigns: ",
+#    [
+#      [[[[] | "&lt;"], "script" | "&gt;"], "badThings();" | "&lt;"],
+#      "/script" |
+#      "&gt;"
+#    ],
+#    "\nThis is the message: ",
+#    "Hello from the view!",
+#    "\n"
+#  ]}
+
+#  rendering the string, without the whole tuple, we can use the render_to_iodata/3.
+# iex(20)> Phoenix.View.render_to_iodata(HelloWeb.PageView, "test.html", message: "Assigns has an @.")
+# ["I came from assigns: ", "Assigns has an @.", "\nThis is the message: ",
+#  "Hello from the view!", "\n"]
